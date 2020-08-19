71315c6f948ac92cdb9e4c0d0eac8510c2c038d4
divrt-roopesha <51081341+divrt-roopesha@users.noreply.github.com>
2020-08-19 22:30:08 +0530

    Added support to entry & exit buttons

--- app/src/main/java/co/divrt/pinasampleapp/MainActivity.java
+++ app/src/main/java/co/divrt/pinasampleapp/MainActivity.java
@@ -15,23 +15,38 @@ import co.divrt.pinasdk.pina.PinaInterface;
 import co.divrt.pinasdk.pina.PinaSdk;
 
 public class MainActivity extends AppCompatActivity {
-    PinaSdk divrtPinaSdk;
+    PinaSdk divrtPinaSdkIn;
+    PinaSdk divrtPinaSdkOut;
+
+    String inOrOut = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        PinaConfig pinaConfig = new PinaConfig();
-        pinaConfig.setVehicleNumber("FAE6137");
-        pinaConfig.setState("New York");
-        pinaConfig.setZoneId("12345");
-        pinaConfig.setInOrOut("IN");
-        pinaConfig.setSimulationMode(true);
-        pinaConfig.setDivrtClientKey("DIVRT_KEY");
-        pinaConfig.setHelpText("Welcome to Commerce Garage");
+        PinaConfig pinaConfigEntry = new PinaConfig();
+        pinaConfigEntry.setVehicleNumber("FAE6137");
+        pinaConfigEntry.setState("New York");
+        pinaConfigEntry.setZoneId("12345");
+        pinaConfigEntry.setInOrOut("IN");
+        pinaConfigEntry.setSimulationMode(true);
+        pinaConfigEntry.setDivrtClientKey("DIVRT_KEY");
+        pinaConfigEntry.setHelpText("Welcome to Commerce Garage");
 
-        divrtPinaSdk = new PinaSdk(getApplicationContext(),MainActivity.this,pinaConfig);
+        divrtPinaSdkIn = new PinaSdk(getApplicationContext(),MainActivity.this,pinaConfigEntry);
+
+
+        PinaConfig pinaConfigExit = new PinaConfig();
+        pinaConfigExit.setVehicleNumber("FAE6137");
+        pinaConfigExit.setState("New York");
+        pinaConfigExit.setZoneId("12345");
+        pinaConfigExit.setInOrOut("OUT");
+        pinaConfigExit.setSimulationMode(true);
+        pinaConfigExit.setDivrtClientKey("DIVRT_KEY");
+        pinaConfigExit.setHelpText("Thank you for exiting Commerce B Garage");
+
+        divrtPinaSdkOut = new PinaSdk(getApplicationContext(),MainActivity.this,pinaConfigExit);
 
     }
 
@@ -39,12 +54,59 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         Intent intent = new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        divrtPinaSdk.onResume(intent);
+        if(inOrOut.equals("IN")){
+            divrtPinaSdkIn.onResume(intent);
+        }else if(inOrOut.equals("OUT")){
+            divrtPinaSdkOut.onResume(intent);
+        }
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(inOrOut.equals("IN")){
+            divrtPinaSdkIn.onResult(requestCode, resultCode, data);
+        }else if(inOrOut.equals("OUT")){
+            divrtPinaSdkOut.onResult(requestCode, resultCode, data);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(inOrOut.equals("IN")){
+            divrtPinaSdkIn.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }else if(inOrOut.equals("OUT")){
+            divrtPinaSdkOut.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(inOrOut.equals("IN")){
+            divrtPinaSdkIn.onPause();
+        }else if(inOrOut.equals("OUT")){
+            divrtPinaSdkOut.onPause();
+        }
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        if(inOrOut.equals("IN")){
+            divrtPinaSdkIn.onNewIntent(intent);
+        }else if(inOrOut.equals("OUT")){
+            divrtPinaSdkOut.onNewIntent(intent);
+        }
     }
 
     public void onClickEntry(View view) {
 
-        divrtPinaSdk.gateHandler(new PinaInterface() {
+        inOrOut = "IN";
+        divrtPinaSdkIn.gateHandler(new PinaInterface() {
             @Override
             public void onSuccess() {
                 Toast.makeText(getBaseContext(),"In Gate Opened",Toast.LENGTH_SHORT).show();
@@ -55,31 +117,23 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getBaseContext(),message,Toast.LENGTH_SHORT).show();
             }
         });
-
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        divrtPinaSdk.onPause();
-    }
+    public void onClickExit(View view) {
 
-    @Override
-    protected void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
-        divrtPinaSdk.onNewIntent(intent);
-    }
+        inOrOut = "OUT";
+        divrtPinaSdkOut.gateHandler(new PinaInterface() {
+            @Override
+            public void onSuccess() {
+                Toast.makeText(getBaseContext(),"Out Gate Opened",Toast.LENGTH_SHORT).show();
+            }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        divrtPinaSdk.onResult(requestCode, resultCode, data);
-    }
+            @Override
+            public void onFailure(String message) {
+                Toast.makeText(getBaseContext(),message,Toast.LENGTH_SHORT).show();
+            }
+        });
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        divrtPinaSdk.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
 
